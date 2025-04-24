0f4b0e8892750f4d56ce23d9c8cc29dd0959ce8c
FrontEndDev-Jagdish <132842416+FrontEndDev-Jagdish@users.noreply.github.com>
2025-04-24 12:55:02 +0530

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -14,7 +14,7 @@ const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
-    color: (theme.vars ?? theme).palette.text.secondary,
+    color: theme.palette.text.secondary,
     ...theme.applyStyles('dark', {
         backgroundColor: '#1A2027',
     }),
